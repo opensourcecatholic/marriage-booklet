8e19df3ef7ff9fb676421e1e6bfa68d70467fcad
John R. D'Orazio <priest@johnromanodorazio.com>
2021-06-06 18:22:33 +0200

    set locale based on request param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { locale: I18n.locale }
   end
+
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
 end
