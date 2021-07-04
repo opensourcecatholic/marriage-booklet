4b67b803511b583ac111379fc02dde8508d66198
John D'Orazio <priest@johnromanodorazio.com>
2021-07-04 16:31:50 +0200

    add locale param to projects test

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -6,18 +6,18 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get projects_url
+    get projects_url, locale: 'en'
     assert_response :success
   end
 
   test "should get new" do
-    get new_project_url
+    get new_project_url, locale: 'en'
     assert_response :success
   end
 
   test "should create project" do
     assert_difference('Project.count') do
-      post projects_url, params: { project: { brideFirstName: @project.brideFirstName, brideLastName: @project.brideLastName, celebrantFirstName: @project.celebrantFirstName, celebrantLastName: @project.celebrantLastName, celebrantNamePrefix: @project.celebrantNamePrefix, church: @project.church, city: @project.city, groomFirstName: @project.groomFirstName, groomLastName: @project.groomLastName, liturgy: @project.liturgy, weddingdate: @project.weddingdate } }
+      post projects_url, locale: 'en', params: { project: { brideFirstName: @project.brideFirstName, brideLastName: @project.brideLastName, celebrantFirstName: @project.celebrantFirstName, celebrantLastName: @project.celebrantLastName, celebrantNamePrefix: @project.celebrantNamePrefix, church: @project.church, city: @project.city, groomFirstName: @project.groomFirstName, groomLastName: @project.groomLastName, liturgy: @project.liturgy, weddingdate: @project.weddingdate } }
     end
 
     assert_redirected_to project_url(Project.last)
@@ -25,22 +25,22 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
 
   test "should show project" do
     get project_url(@project)
-    assert_response :success
+    assert_response :success, locale: 'en'
   end
 
   test "should get edit" do
     get edit_project_url(@project)
-    assert_response :success
+    assert_response :success, locale: 'en'
   end
 
   test "should update project" do
-    patch project_url(@project), params: { project: { brideFirstName: @project.brideFirstName, brideLastName: @project.brideLastName, celebrantFirstName: @project.celebrantFirstName, celebrantLastName: @project.celebrantLastName, celebrantNamePrefix: @project.celebrantNamePrefix, church: @project.church, city: @project.city, groomFirstName: @project.groomFirstName, groomLastName: @project.groomLastName, liturgy: @project.liturgy, weddingdate: @project.weddingdate } }
+    patch project_url(@project), locale: 'en', params: { project: { brideFirstName: @project.brideFirstName, brideLastName: @project.brideLastName, celebrantFirstName: @project.celebrantFirstName, celebrantLastName: @project.celebrantLastName, celebrantNamePrefix: @project.celebrantNamePrefix, church: @project.church, city: @project.city, groomFirstName: @project.groomFirstName, groomLastName: @project.groomLastName, liturgy: @project.liturgy, weddingdate: @project.weddingdate } }
     assert_redirected_to project_url(@project)
   end
 
   test "should destroy project" do
     assert_difference('Project.count', -1) do
-      delete project_url(@project)
+      delete project_url(@project), locale: 'en'
     end
 
     assert_redirected_to projects_url
