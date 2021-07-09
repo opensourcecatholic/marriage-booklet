3a2ba5940870aa0973e5d6237d9059985be3972f
JohnRDOrazio <priest@johnromanodorazio.org>
2021-07-09 13:18:09 +0200

    as per David Teren blog

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,24 @@
 const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
 
+// Get the actual sass-loader config
+const sassLoader = environment.loaders.get('sass')
+const sassLoaderConfig = sassLoader.use.find(function (element) {
+return element.loader == 'sass-loader'
+})
+
+// Use Dart-implementation of Sass (default is node-sass)
+const options = sassLoaderConfig.options
+options.implementation = require('sass')
+
+function hotfixPostcssLoaderConfig(subloader) {
+  const subloaderName = subloader.loader
+  if (subloaderName === 'postcss-loader') {
+    subloader.options.postcssOptions = subloader.options.config
+    delete subloader.options.config
+  }
+}
+
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
@@ -11,4 +29,10 @@ environment.plugins.prepend('Provide',
 )
 
 environment.loaders.prepend('erb', erb)
+
+environment.loaders.keys().forEach(loaderName => {
+  const loader = environment.loaders.get(loaderName)
+  loader.use.forEach(hotfixPostcssLoaderConfig)
+})
+
 module.exports = environment
