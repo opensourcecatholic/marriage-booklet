9ccaf0f3f88071a9cf4f3f140e055f8edd290754
John R. D'Orazio <priest@johnromanodorazio.com>
2021-07-18 12:01:58 +0200

    add homepage test, add img

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -35,3 +35,6 @@ import('src/translations').then(() => {
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+const images = require.context("../images", true)
+const imagePath = name => images(name, true)
