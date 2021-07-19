bce157ec7f075184f065eb83961d357b5f7e9628
John R. D'Orazio <priest@johnromanodorazio.com>
2021-07-19 08:05:42 +0200

    account for orphaned projects / users

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ApplicationRecord
   has_and_belongs_to_many :users
   has_many :wedding_party_members, dependent: :delete_all
   enum liturgy: { withMass: 0, withoutMass: 1, catholicNonCatholic: 2 }, _prefix: true
+  scope :orphaned, -> {
+    left_outer_joins(:users)
+    .where(projects_users: { project_id: nil })
+  }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_and_belongs_to_many :projects
   enum role: { bride: 0, groom: 1, celebrant: 2, guest: 3 }, _prefix: true
   has_secure_password
+  scope :not_in_any_project, -> {
+    left_outer_joins(:projects)
+    .where(projects_users: { user_id: nil })
+  }
 end
